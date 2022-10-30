ac3f515bbcf5ef2a7fd487e5a2dbf96da8288da1
Nortiz <nortiz1349@gmail.com>
2022-10-30 22:25:29 +0900

    Initial Commit

---
+++ SwiftUIAdvancedLearning/SwiftUIAdvancedLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAdvancedLearningApp.swift
+//  SwiftUIAdvancedLearning
+//
+//  Created by Nortiz M1 on 2022/10/30.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAdvancedLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
