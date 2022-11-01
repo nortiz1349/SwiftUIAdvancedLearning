966ee9303063989dd225e25129495a5fc2d6d1a6
Nortiz <nortiz1349@gmail.com>
2022-11-01 11:18:37 +0900

    Custom TabBar with Generics, ViewBuilder, PreferenceKey, MatchedGeometryEffect

---
+++ SwiftUIAdvancedLearning/CustomTabBar/CustomTabBarContainerView.swift
@@ -0,0 +1,42 @@
+//
+//  CustomTabBarContainerView.swift
+//  SwiftUIAdvancedLearning
+//
+//  Created by Nortiz M1 on 2022/11/01.
+//
+
+import SwiftUI
+
+struct CustomTabBarContainerView<Content:View>: View {
+	
+	@Binding var selection: TabBarItem
+	let content: Content
+	@State private var tabs: [TabBarItem] = []
+	
+	init(selection: Binding<TabBarItem>, @ViewBuilder content: () -> Content) {
+		self._selection = selection
+		self.content = content()
+	}
+	
+    var body: some View {
+		ZStack(alignment: .bottom) {
+			content
+				.ignoresSafeArea()
+			CustomTabBarView(tabs: tabs, selection: $selection, localSelection: selection)
+		}
+		.onPreferenceChange(TabBarItemsPreferenceKey.self) { value in
+			self.tabs = value
+		}
+    }
+}
+
+struct CustomTabBarContainerView_Previews: PreviewProvider {
+	
+	static let tabs: [TabBarItem] = [.home, .favorites, .profile]
+	
+    static var previews: some View {
+		CustomTabBarContainerView(selection: .constant(tabs.first!)) {
+			Color.red
+		}
+    }
+}
---
+++ SwiftUIAdvancedLearning/CustomTabBar/TabBarItem.swift
@@ -0,0 +1,46 @@
+//
+//  TabBarItem.swift
+//  SwiftUIAdvancedLearning
+//
+//  Created by Nortiz M1 on 2022/11/01.
+//
+
+import Foundation
+import SwiftUI
+
+//struct TabBarItem: Hashable {
+//	let iconName: String
+//	let title: String
+//	let color: Color
+//}
+
+enum TabBarItem: Hashable {
+	case home, favorites, profile, messages
+	
+	var iconName: String {
+		switch self {
+		case .home: return "house"
+		case .favorites: return "heart"
+		case .profile: return "person"
+		case .messages: return "message"
+		}
+	}
+	
+	var title: String {
+		switch self {
+		case .home: return "Home"
+		case .favorites: return "Favorite"
+		case .profile: return "Profile"
+		case .messages: return "Messages"
+		}
+	}
+	
+	var color: Color {
+		switch self {
+		case .home: return Color.red
+		case .favorites: return Color.blue
+		case .profile: return Color.green
+		case .messages: return Color.orange
+		}
+	}
+}
